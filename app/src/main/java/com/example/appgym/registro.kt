40ebf82bd3e150d629ca1876428d6470bb607924
Carlos <carlos200012-@hotmail.com>
2023-01-08 20:05:10 -0500

    "Actualizado"

--- app/src/main/java/com/example/appgym/registro.kt
+++ app/src/main/java/com/example/appgym/registro.kt
@@ -1,11 +1,21 @@
 package com.example.appgym
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class registro : AppCompatActivity() {
+    lateinit var BtnVolver:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+        BtnVolver=findViewById(R.id.btnVolver)
+        BtnVolver.setOnClickListener {
+            //val form = Intent(this@registro, MainActivity::class.java)
+            //startActivity(form)
+            finish()
+        }
     }
 }
